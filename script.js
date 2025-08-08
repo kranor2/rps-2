2309de043218b3fbc75b825f46264aa49365e91e
Rhett North <retsrawk@gmail.com>
2025-08-08 15:11:18 -0600

    Computer participation

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ let computerPoints = 0
 const roundMarker = document.getElementById("roundMarker");
 const playerScore = document.getElementById("playerScore");
 const computerScore = document.getElementById("computerScore");
+
+// set up computer's game participation
+function getComputerChoice(min, max) {
+    return Math.floor(Math.random() * (5 - 1 + 1) + 1);
+}
+let computerChoice = getComputerchoice(1, 5);
+
+function getComputerSelection() {
+    if (computerChoice == 1) {
+        return "rock"
+    } else if (computerChoice == 2) {
+        return "paper"
+    } else if (computerChoice == 3) {
+        return "scissors"
+    } else if (computerChoice == 4) {
+        return "lizard"
+    } else if (computerChoice == 5) {
+        return "spock"
+    } else {
+        return "forfeit"
+    }
+};
+
+let computerSelection = getComputerSelection("rock" | "paper" | "scissors" | "lizard" | "spock");
