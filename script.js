2398830e3ec3f3bed1cc8e4a2f44f8e75766cba9
Rhett North <retsrawk@gmail.com>
2025-08-08 15:07:08 -0600

    Create script, initialised variables

---
+++ script.js
@@ -0,0 +1,10 @@
+// Rock Paper Scissors Lizard Spock
+
+// initalise variables, constants for altered text content
+let currentRound = 0
+let playerPoints = 0
+let computerPoints = 0
+
+const roundMarker = document.getElementById("roundMarker");
+const playerScore = document.getElementById("playerScore");
+const computerScore = document.getElementById("computerScore");
